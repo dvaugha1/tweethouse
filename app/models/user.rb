8e17ae141ca831216044266580e7fca1181bd2b8
Mike Vaughan <dvaugha1@gmail.com>
2015-02-11 14:59:02 -0500

    Added relationships in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,25 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :shouts
 
+  has_many :relationships, foreign_key: "follower_id"
+  has_many :following, through: :relationships, source: :followed
+
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship"
+  has_many :followers, through: :reverse_relationships, source: :follower
+
 	validates :username, presence: true, uniqueness: true,
-	  format: { with: /[a-zA-Z0-9]{4,20}}/,
-	  message: "must be between 4 and 20 alphanumerics"} 
+	  format: { with: /[a-zA-Z0-9]{4,20}/,
+	  message: "must be between 4 and 20 alphanumerics"}
+
+  def follow(user)
+    self.following.where(:followed_id => user.id).first_or_create!
+  end
+
+  def unfollow(user)
+    self.following.where(:followed_id => user.id).destroy!
+  end
+
+  def follows?(user)
+    self.following.include?(user)
+  end
 end
